e84270aefa5373bddc2290e8d7b3d4c2fe463301
diana_4881 <dianamoyalapeira@gmail.com>
2024-06-22 16:09:06 -0400

    Initial Commit

---
+++ UmbrellaFloodSafety/UmbrellaFloodSafetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmbrellaFloodSafetyApp.swift
+//  UmbrellaFloodSafety
+//
+//  Created by Diana Moya Lapeira on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct UmbrellaFloodSafetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
